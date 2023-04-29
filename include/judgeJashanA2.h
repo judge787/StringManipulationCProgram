e5c96812635f1b33de311c1891c1ca8fcc6afede
Jashan Judge <125755912+judge787@users.noreply.github.com>
2023-04-28 20:06:03 -0400

    Update judgeJashanA2.h

--- include/judgeJashanA2.h
+++ include/judgeJashanA2.h
@@ -1,17 +1,4 @@
-/*
-Student Name: Jashan Judge
-Student ID: 1150301
-Due Date: Monday March 6
-Course: CIS*2500
-I have exclusive control over this submission via my password.
-By including this header comment, I certify that:
-1) I have read and understood the policy on academic integrity.
-2) I have completed Moodle's module on academic integrity.
-3) I have achieved at least 80% on the academic integrity quiz
-I Jashan Judge assert that this work is my own. I have appropriate acknowledged
-any and all material that I have used, be it directly quoted or
-paraphrased. Furthermore, I certify that this assignment was written
-by me in its entirety.*/
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
